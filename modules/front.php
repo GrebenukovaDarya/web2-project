5dfa3e2e3685576cdd4be08316806ea2f8aeb430
s0188223 <loved4248@gmail.com>
2025-05-12 16:21:23 +0300

    с

--- modules/front.php
+++ modules/front.php
@@ -158,12 +158,14 @@ function front_get($request, $db) {
 
 // Обработчик запросов методом POST.
 function front_post($request, $db) {
+
+  /*
   $is_ajax = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&
   strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
 
   if ($is_ajax) {
     header('Content-Type: application/json');
-  }
+  }*/
 
   if (!validateCsrfToken()) {
     // if($is_ajax){
