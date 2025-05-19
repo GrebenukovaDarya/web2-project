6971c31aa87821335a88c10d23aa1c337604dd2f
s0188223 <loved4248@gmail.com>
2025-05-19 17:17:42 +0300

    c

--- modules/logout.php
+++ modules/logout.php
@@ -2,7 +2,7 @@
 require_once './scripts/db.php';
 global $db;
    function logout_post($request, $db) {
-        console.log("wenvkwe");
+    
        if (isset($request['post']['logout'])) {
         echo "wiebnwoe woegw";
            session_unset();
